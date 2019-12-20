496556394474376b710d3b216702dc65d89445ba
root <root@66.98.120.115.16clouds.com>
2019-12-20 18:15:29 -0500

    update list

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+import puppeteer from 'puppeteer';
+import fs from 'fs';
 (async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
