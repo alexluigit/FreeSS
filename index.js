b5b33c7d26e790ac10882631d29feea6b74c382c
root <root@66.98.120.115.16clouds.com>
2019-12-20 17:35:35 -0500

    generate list.txt

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const puppeteer = require('puppeteer');
-
+import puppeteer from 'puppeteer';
+import fs from 'fs';
 (async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://lncn.org/', {waitUntil: 'networkidle2'});
   const btns = await page.$$('div.ssrwrap > div > div > div > div > button')
   let list = []
-  for (btn of btns){
+  for (let btn of btns){
     await btn.click()
     const lk = await page.waitForResponse(respond => respond)
     await page.waitFor(500)
     list.push(await lk.text())
   }
-  console.log(list)
+  fs.writeFile('list.txt', list.join('\n'), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
   await browser.close();
 })();
 
